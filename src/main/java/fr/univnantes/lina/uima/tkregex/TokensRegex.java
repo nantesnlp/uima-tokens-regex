1e7e3ffcf9b241e2ca9c484ba40e18cbb9a48bac
Damien Cram <dcr@sparklane.fr>
2017-11-10 18:03:36 +0100

    externalized quantifier parsing

---
+++ src/main/java/fr/univnantes/lina/uima/tkregex/TokensRegex.java
@@ -0,0 +1,41 @@
+package fr.univnantes.lina.uima.tkregex;
+
+import fr.univnantes.lina.uima.tkregex.antlr.AutomataParsingException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TokensRegex {
+	private static final Pattern M_N_PATTERN = Pattern.compile("\\{(\\d+),(\\d+)\\}");
+	private static final Pattern N_PATTERN = Pattern.compile("\\{(\\d+)\\}");
+
+
+	public static AutomatonQuantifier parseQuantifier(String quantifierString) {
+		AutomatonQuantifier quantifier = null;
+		switch(quantifierString) {
+			case "*":
+				quantifier = AutomatonQuantifier.getZeroN();
+				break;
+			case "?":
+				quantifier = AutomatonQuantifier.getZeroOne();
+				break;
+			case "+":
+				quantifier = AutomatonQuantifier.getOneN();
+				break;
+			default:
+				Matcher m_n_matcher = M_N_PATTERN.matcher(quantifierString);
+				Matcher n_matcher = N_PATTERN.matcher(quantifierString);
+				if(m_n_matcher.matches())
+					quantifier = AutomatonQuantifier.getMN(
+							Integer.parseInt(m_n_matcher.group(1)),
+							Integer.parseInt(m_n_matcher.group(2))
+					);
+				else if(n_matcher.matches())
+					quantifier = AutomatonQuantifier.getN(
+							Integer.parseInt(n_matcher.group(1))
+					);
+				else throw new AutomataParsingException("Unrecognized regex: " + quantifierString);
+		}
+		return quantifier;
+	}
+}
--- src/main/java/fr/univnantes/lina/uima/tkregex/antlr/AutomataParserListener.java
+++ src/main/java/fr/univnantes/lina/uima/tkregex/antlr/AutomataParserListener.java
@@ -65,8 +65,6 @@ public class AutomataParserListener implements UimaTokenRegexListener {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(AutomataParserListener.class);
 
-	private static final Pattern M_N_PATTERN = Pattern.compile("\\{(\\d+),(\\d+)\\}");
-	private static final Pattern N_PATTERN = Pattern.compile("\\{(\\d+)\\}");
 
 	public static final String OPTION_INLINE = "inline";
 	
@@ -479,32 +477,9 @@ public class AutomataParserListener implements UimaTokenRegexListener {
 	}
 
 	private AutomatonQuantifier parseQuantifier(QuantifierDeclarationContext ctx) {
-		AutomatonQuantifier quantifier = null;
-		switch(ctx.getText()) {
-		case "*":
-			quantifier = AutomatonQuantifier.getZeroN();
-			break;
-		case "?":
-			quantifier = AutomatonQuantifier.getZeroOne();
-			break;
-		case "+":
-			quantifier = AutomatonQuantifier.getOneN();
-			break;
-		default:
-			Matcher m_n_matcher = M_N_PATTERN.matcher(ctx.getText());
-			Matcher n_matcher = N_PATTERN.matcher(ctx.getText());
-			if(m_n_matcher.matches())
-				quantifier = AutomatonQuantifier.getMN(
-						Integer.parseInt(m_n_matcher.group(1)),
-						Integer.parseInt(m_n_matcher.group(2))
-						);
-			else if(n_matcher.matches())
-				quantifier = AutomatonQuantifier.getN(
-						Integer.parseInt(n_matcher.group(1))
-						);
-			else throw new AutomataParsingException("Unrecognized regex: " + ctx.getText());
-		}
-		return quantifier;
+		String quantifierString = ctx.getText();
+
+		return TokensRegex.parseQuantifier(quantifierString);
 	}
 
 	@Override
